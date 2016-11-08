4cd33502dba085cd513c66294ba24b362d964821
nicmak <nicholas123mak@gmail.com>
2016-11-08 19:49:27 +0000

    Avatar Assignment Submit

--- download_avatars.js
+++ download_avatars.js
@@ -1,38 +1,50 @@
+const dotEnv = require('dotenv')
+dotEnv.config();
+const username = process.env.USER
+const password = process.env.TOKEN
 
+
+
+//require('dotenv').config()
 console.log('Welcome to the Github Avatar Downloader!');
-var request = require('request');
-var fs = require('fs');
-var input = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+const input = process.argv.slice(2);
+
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var GITHUB_USER = "nicmak";
-  var GITHUB_TOKEN = ""; //Email nicholas123mak@gmail.com for token 
-  var requestURL="https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  const requestURL="https://" + username + ":" + password + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
 
-  var options = {url: requestURL, headers: {'User-Agent' : "GitHub Avatar-Student Project"}};
-  var callback = function(err, response, body) {
-    cb(err, JSON.parse(body));
+  const options = {
+    url: requestURL,
+    headers: {'User-Agent' : "GitHub Avatar-Student Project"}
   };
+  // const callback = function(err, response, body) {
+  //   cb(err, JSON.parse(body));
+  //};
 
-  request(options, callback);
+  request(options, cb);
 };
 
 function downloadImageByURL(url, filePath) { //use url as request URL
   //fs.mkdirSync("avatars");
   request.get(url)
-         .on('error', function (err) {
-          throw err;
-         })
-         .pipe(fs.createWriteStream("Avatars/"+ filePath + ".jpg")); 
-  
+    .on('error', function (err) {
+    throw err;
+    })
+    .pipe(fs.createWriteStream("Avatars/"+ filePath + ".jpg"));
+
 };
 
 
-getRepoContributors(input[0], input[1], function(err, result) {
-     console.log("Errors:", err);
-  result.forEach(function(contri) {   //avatar represents result[i], loops over contributors and all their information
-     console.log("Avatar_URL: ",contri['avatar_url'])
-     downloadImageByURL(contri['avatar_url'], contri['login'])
+getRepoContributors(input[0], input[1], function(err, response, body) {
+  const bodyJSON = JSON.parse(body);
+  console.log("Errors:", err);
+  bodyJSON.forEach(function(contrib) {   //avatar represents result[i], loops over contributors and all their information
+     console.log("Avatar_URL: ",contrib['avatar_url'])
+     downloadImageByURL(contrib['avatar_url'], contrib['login'])
   })
   //console.log("Result:", result);
 });
