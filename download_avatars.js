93e1167596345d55417d392abc0efccfc89ccbe8
nicmak <nicholas123mak@gmail.com>
2016-11-08 14:01:03 +0000

    Part1complete

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,11 @@
 console.log('Welcome to the Github Avatar Downloader!');
 var request = require('request');
 var fs = require('fs');
+var input = process.argv.slice(2);
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_USER = "nicmak";
-  var GITHUB_TOKEN = "78e5f7f89507dc9ed0249387103067f5368e13e7";
+  var GITHUB_TOKEN = ""; //Email nicholas123mak@gmail.com for token 
   var requestURL="https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
 
   var options = {url: requestURL, headers: {'User-Agent' : "GitHub Avatar-Student Project"}};
@@ -22,12 +23,12 @@ function downloadImageByURL(url, filePath) { //use url as request URL
          .on('error', function (err) {
           throw err;
          })
-         .pipe(fs.createWriteStream("avatars/"+ filePath + ".jpg")); 
+         .pipe(fs.createWriteStream("Avatars/"+ filePath + ".jpg")); 
   
 };
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(input[0], input[1], function(err, result) {
      console.log("Errors:", err);
   result.forEach(function(contri) {   //avatar represents result[i], loops over contributors and all their information
      console.log("Avatar_URL: ",contri['avatar_url'])
